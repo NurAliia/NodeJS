89afca4c49eae3ca41d87de001c3160f0275a7ad
NurAliia <battalova.nur@gmail.com>
2020-06-04 00:08:16 +0400

    implemented task_1_1, task_1_2

---
+++ task_1/task_1_2.js
@@ -0,0 +1,20 @@
+const csvFilePath="./task_1/example.csv";
+const csvtojsonV2=require("csvtojson/v2");
+const fs=require("fs");
+
+csvtojsonV2()
+  .fromFile(csvFilePath)
+  .then((jsonObj)=>{
+    writeToFile(jsonObj);
+  })
+
+function writeToFile(jsonObj){
+  let text='';
+  jsonObj.forEach(item => {
+    text+=JSON.stringify(item) + '\n';
+  })
+  fs.writeFile("./task_1/output.txt", text, (err) => {
+    if (err) {  console.error(err);  return; };
+    console.log("File has been created");
+  })      
+}
